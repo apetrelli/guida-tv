23dedb293973ca23b11fb8a499ecb90530bf4213
antonio.petrelli <antonio.petrelli@a2f57c54-68f8-aa68-6b43-d9ab637a1aa9>
2011-02-04 19:45:44 +0000

    Recovered la7d.

--- guidatv-gae/src/com/google/code/guidatv/server/service/impl/italy/generalistic/dao/TelecomTransmissionDao.java
+++ guidatv-gae/src/com/google/code/guidatv/server/service/impl/italy/generalistic/dao/TelecomTransmissionDao.java
@@ -2,11 +2,11 @@ package com.google.code.guidatv.server.service.impl.italy.generalistic.dao;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
+import com.google.code.guidatv.client.model.Channel;
 import com.google.code.guidatv.client.model.Transmission;
 
 public interface TelecomTransmissionDao {
 
-    Map<String, List<Transmission>> getTransmissions(Date day);
+    List<Transmission> getTransmissions(Channel channel, Date day);
 }
