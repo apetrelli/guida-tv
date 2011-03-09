b1bb0b9daf759efeacb5f50d290d52c418171f66
antonio.petrelli <antonio.petrelli@a2f57c54-68f8-aa68-6b43-d9ab637a1aa9>
2011-03-09 18:50:03 +0000

    First stub of calling a differenct activity.

--- guidatv-android/src/com/google/code/guidatv/android/GuidaTv.java
+++ guidatv-android/src/com/google/code/guidatv/android/GuidaTv.java
@@ -1,62 +1,37 @@
 package com.google.code.guidatv.android;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.util.Collection;
-import java.util.List;
-
-import org.codehaus.jackson.annotate.JsonAutoDetect.Visibility;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.introspect.VisibilityChecker;
-import org.codehaus.jackson.type.TypeReference;
-import org.restlet.data.MediaType;
-import org.restlet.engine.Engine;
-import org.restlet.ext.jackson.JacksonConverter;
-import org.restlet.representation.Representation;
-import org.restlet.resource.ClientResource;
-
-import android.app.ListActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-import com.google.code.guidatv.model.Channel;
+public class GuidaTv extends Activity {
 
-public class GuidaTv extends ListActivity {
+    private static final int ACTIVITY_CHANNELS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.channel_list);
-        Engine.getInstance().getRegisteredConverters().add(new JacksonConverter());
-        ClientResource resource = new ClientResource("http://10.0.2.2:8888/rest/channels");
-        Representation representation = resource.get(MediaType.APPLICATION_JSON);
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setVisibilityChecker(new VisibilityChecker.Std(
-                Visibility.DEFAULT, Visibility.DEFAULT, Visibility.DEFAULT,
-                Visibility.DEFAULT, Visibility.ANY));
-        Reader reader;
-        List<Channel> channels;
-        try
+        setContentView(R.layout.main);
+        Button channelsButton = (Button) findViewById(R.id.channelsButton);
+        channelsButton.setOnClickListener(new OnClickListener()
         {
-            reader = representation.getReader();
-            channels = mapper.readValue(reader, new TypeReference<List<Channel>>() {});
-            reader.close();
-        } catch (IOException e)
-        {
-            Log.e("guida tv", "errore IO", e);
-            throw new RuntimeException(e);
-        }
-        String[] channelArray = new String[channels.size()];
-        int i = 0;
-        for (Channel channel : channels) {
-            channelArray[i] = channel.getName();
-            i++;
-        }
-        ListAdapter adapter = new ArrayAdapter<String>(this, R.layout.channel_item, R.id.channel_text, channelArray);
-        setListAdapter(adapter);
+
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(GuidaTv.this, ChannelsView.class);
+                startActivityForResult(i, ACTIVITY_CHANNELS);
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
