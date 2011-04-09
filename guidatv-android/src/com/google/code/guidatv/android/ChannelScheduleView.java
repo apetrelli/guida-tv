645b14288dfcf6580a65a25a6e86f9e522ae63f6
antonio.petrelli <antonio.petrelli@a2f57c54-68f8-aa68-6b43-d9ab637a1aa9>
2011-04-09 11:50:37 +0000

    Scrolling to current transmission.

--- guidatv-android/src/com/google/code/guidatv/android/ChannelScheduleView.java
+++ guidatv-android/src/com/google/code/guidatv/android/ChannelScheduleView.java
@@ -3,6 +3,7 @@ package com.google.code.guidatv.android;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -88,11 +89,14 @@ public class ChannelScheduleView extends ListActivity
 
     private class LoadScheduleTask extends AsyncTask<Serializable, Void, Schedule> {
 
-        @Override
+        private Date requestedDate;
+
+		@Override
         protected Schedule doInBackground(Serializable... params)
         {
 			try {
-				return mGuidaTvService.getSchedule((String) params[0], (Date) params[1]);
+				requestedDate = (Date) params[1];
+				return mGuidaTvService.getSchedule((String) params[0], requestedDate);
 			} catch (IOException e) {
 				Log.d("ChannelScheduleView", "Cannot retrieve schedule for "
 						+ params[0] + ", are you connected to Internet", e);
@@ -126,21 +130,39 @@ public class ChannelScheduleView extends ListActivity
 				empty.setText("Unreadable schedule");
         		return;
         	}
-            List<Map<String, String>> channelList = new ArrayList<Map<String,String>>();
+        	Calendar nowCalendar = Calendar.getInstance();
+			Date now = nowCalendar.getTime();
+        	Calendar requestedDateCalendar = nowCalendar;
+        	requestedDateCalendar.setTime(requestedDate);
+        	boolean isToday = nowCalendar.get(Calendar.YEAR) == requestedDateCalendar.get(Calendar.YEAR) &&
+        	nowCalendar.get(Calendar.MONTH) == requestedDateCalendar.get(Calendar.MONTH) &&
+        	nowCalendar.get(Calendar.DATE) == requestedDateCalendar.get(Calendar.DATE);
+        	int index = 0;
+            List<Map<String, String>> transmissionList = new ArrayList<Map<String,String>>();
             for (Transmission transmission : schedule.getTransmissions()) {
-                Map<String, String> channelMap = new HashMap<String, String>();
-                channelMap.put("INFO", transmission.getDescription());
-                channelMap.put("TIME_AND_NAME",
+                Map<String, String> transmissionMap = new HashMap<String, String>();
+                transmissionMap.put("INFO", transmission.getDescription());
+                transmissionMap.put("TIME_AND_NAME",
                         DateFormat.format("kk:mm", transmission.getStart())
                                 .toString() + " " + transmission.getName());
-                channelList.add(channelMap);
+                transmissionList.add(transmissionMap);
+                if (now.after(transmission.getStart())) {
+                	index++;
+                }
             }
             SimpleAdapter adapter = new SimpleAdapter(
-                    ChannelScheduleView.this, channelList, R.layout.transmission_item,
+                    ChannelScheduleView.this, transmissionList, R.layout.transmission_item,
                     new String[]
                     { "TIME_AND_NAME", "INFO" }, new int[]
                     { android.R.id.text1, android.R.id.text2 });
             setListAdapter(adapter);
+            if (isToday) {
+            	if (index > 0) {
+            		index--;
+            	}
+            	ListView listView = (ListView) findViewById(android.R.id.list);
+            	listView.setSelection(index);
+            }
         }
     }
 	
