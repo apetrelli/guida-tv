f62a8ea6191158ac6442377bb4da4572d5558f40
antonio.petrelli@gmail.com <antonio.petrelli@gmail.com@a2f57c54-68f8-aa68-6b43-d9ab637a1aa9>
2012-05-30 19:53:34 +0000

    Added commons-digester. Changed RealTime completely.

--- guidatv-gae/src/main/java/com/google/code/guidatv/server/service/impl/italy/generalistic/dao/DNERealTimeTransmissionDaoImpl.java
+++ guidatv-gae/src/main/java/com/google/code/guidatv/server/service/impl/italy/generalistic/dao/DNERealTimeTransmissionDaoImpl.java
@@ -6,91 +6,160 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
 import java.text.DecimalFormat;
 import java.text.MessageFormat;
 import java.text.NumberFormat;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Deque;
 import java.util.List;
 import java.util.TimeZone;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.apache.commons.digester3.Digester;
+import org.apache.commons.digester3.Rule;
+import org.cyberneko.html.parsers.SAXParser;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
 
 import com.google.code.guidatv.model.Transmission;
 import com.google.code.guidatv.server.service.GuidaTvException;
 
 public class DNERealTimeTransmissionDaoImpl implements DNERealTimeTransmissionDao {
 
-    private final Logger log = Logger.getLogger(getClass().getName());
-
-    private String pattern = "http://www.realtimetv.it/guidatv/guidatv.php?type={0}&day={1,number,0000}{2,number,00}{3,number,00}";
+    private String pattern = "http://www.realtimetv.it/tvl-fe/day/?type=day&channel_code=RTIT-IT&date={0}&filter=0600&style=hh_day";
 
     public List<Transmission> getTransmissions(Date day) {
         MessageFormat format = new MessageFormat(pattern);
-        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"));
-        cal.setTime(day);
-        List<Transmission> transmissions = new ArrayList<Transmission>();
-        cal.add(Calendar.DATE, -1);
-        loadTransmissions("notte", day, cal.getTime(), format, transmissions);
-        loadTransmissions("mattina", day, day, format, transmissions);
-        loadTransmissions("pomeriggio", day, day, format, transmissions);
-        loadTransmissions("sera", day, day, format, transmissions);
-        return transmissions;
-    }
-
-    private void loadTransmissions(String type, Date day, Date urlDay,
-            MessageFormat format, List<Transmission> transmissions) {
-        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"));
-        cal.setTime(urlDay);
-        String urlString = format.format(new Object[] { type,
-                cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1,
-                cal.get(Calendar.DATE) });
+        TimeZone timeZone = TimeZone.getTimeZone("Europe/Rome");
+        DateFormat dateFormat = new SimpleDateFormat("ddMMyyyy");
+        dateFormat.setTimeZone(timeZone);
+		String urlString = format.format(new Object[] { dateFormat.format(day) });
         Reader reader = null;
+        final List<Transmission> transmissions = new ArrayList<Transmission>();
         try {
             URL url = new URL(urlString);
             InputStream is = url.openStream();
             reader = new InputStreamReader(is, "UTF-8");
-            StringBuilder builder = new StringBuilder();
-            int character;
-            while ((character = reader.read()) > 0) {
-                builder.append((char) character);
-            }
-            JSONObject jsonObject = new JSONObject(builder.toString());
-            JSONArray jsonArray = jsonObject.getJSONArray("contents");
-            DecimalFormat numberFormat = (DecimalFormat) NumberFormat.getInstance();
-            numberFormat.applyPattern("00");
-            boolean sameDay = true;
-            for (int i=0; i < jsonArray.length() && sameDay; i++) {
-                JSONObject transmissionJson = jsonArray.getJSONObject(i);
-                String timeString = transmissionJson.getString("time");
-                String hour = timeString.substring(0, 2);
-                String minutes = timeString.substring(3, 5);
-                int hours = numberFormat.parse(hour).intValue();
-                int minute = numberFormat.parse(minutes).intValue();
-                cal.setTime(day);
-                cal.set(Calendar.HOUR_OF_DAY,hours);
-                cal.set(Calendar.MINUTE, minute);
-                cal.set(Calendar.SECOND, 0);
-                cal.set(Calendar.MILLISECOND, 0);
-                transmissions.add(new Transmission(transmissionJson
-                        .getString("name"), transmissionJson
-                        .getString("description"), cal.getTime(), null, null));
-            }
+            SAXParser parser = new SAXParser();
+            Digester digester = new Digester(parser);
+            final TransmissionBuilder builder = new TransmissionBuilder();
+            builder.setStartingTime(day);
+            final Deque<Boolean> processingStack = new ArrayDeque<Boolean>();
+            digester.addRule("*/TABLE/TBODY/TR", new Rule() {
+            	@Override
+            	public void begin(String namespace, String name,
+            			Attributes attributes) {
+            		String clazz = attributes.getValue("class");
+					boolean toProcess = clazz != null && clazz.contains("listing");
+            		processingStack.push(toProcess);
+            		if (toProcess) {
+            			builder.prepareForNextTransmission();
+            		}
+            	}
+            	@Override
+            	public void end(String namespace, String name) throws Exception {
+            		if (processingStack.pop()) {
+            			transmissions.add(builder.build());
+            		}
+            	}
+			});
+            digester.addRule("*/TABLE/TBODY/TR/TD/TIME/SPAN", new PreprocessingRule(processingStack) {
+            	
+            	private DecimalFormat numberFormat;
+            	
+            	{
+            		numberFormat= (DecimalFormat) NumberFormat.getInstance();
+            		numberFormat.applyPattern("00");
+            	}
+				
+				@Override
+				public boolean isProcessable(Attributes attributes) {
+					return "listing-time".equals(attributes.getValue("class"));
+				}
+            	
+            	@Override
+            	public void body(String namespace, String name, String text) throws ParseException {
+            		if (processingStack.peek()) {
+                        String timeString = text.trim();
+                        String hourString = timeString.substring(0, 2);
+                        String minutesString = timeString.substring(3, 5);
+                        int hour = numberFormat.parse(hourString).intValue();
+                        int minutes = numberFormat.parse(minutesString).intValue();
+                        builder.setTime(hour, minutes);
+            		}
+            	}
+			});
+            
+			digester.addRule("*/TABLE/TBODY/TR/TD/DIV/DIV",
+					new PreprocessingRule(processingStack) {
+
+						@Override
+						public boolean isProcessable(Attributes attributes) {
+							return "module".equals(attributes.getValue("class"));
+						}
+					});
+
+			digester.addRule("*/TABLE/TBODY/TR/TD/DIV/DIV/H3/A", new Rule() {
+				@Override
+				public void body(String namespace, String name, String text)
+						throws Exception {
+					if (processingStack.peek()) {
+						builder.setTitle(text.trim());
+					}
+				}
+			});
+
+			digester.addRule("*/TABLE/TBODY/TR/TD/DIV/DIV/P", new Rule() {
+				
+				private int index = 0;
+				
+				@Override
+				public void begin(String namespace, String name,
+						Attributes attributes) throws Exception {
+					String clazz = attributes.getValue("class");
+					if (clazz != null && clazz.contains("module-content")) {
+						if (clazz.contains("episode-title")) {
+							index = 1;
+						} else {
+							index = 2;
+						}
+					}
+				}
+				
+				@Override
+				public void body(String namespace, String name, String text)
+						throws Exception {
+					if (processingStack.peek() && index > 0) {
+						String trimmedText = text.trim();
+						if (index == 1) {
+							if (!trimmedText.isEmpty()) {
+								builder.setEpisode(trimmedText);
+							}
+						} else {
+							builder.setDescription(trimmedText);
+						}
+					}
+				}
+				
+				@Override
+				public void end(String namespace, String name) throws Exception {
+					index = 0;
+				}
+			});
+            
+            digester.parse(reader);
         } catch (MalformedURLException e) {
             throw new GuidaTvException(e);
         } catch (IOException e) {
             throw new GuidaTvException(e);
-        } catch (ParseException e) {
+        } catch (SAXException e) {
             throw new GuidaTvException(e);
-        } catch (JSONException e) {
-            log.log(Level.FINE, "Exception when getting schedule", e);
-        } finally {
+		} finally {
             if (reader != null) {
                 try {
                     reader.close();
@@ -99,5 +168,91 @@ public class DNERealTimeTransmissionDaoImpl implements DNERealTimeTransmissionDa
                 }
             }
         }
+        return transmissions;
+    }
+    
+    private abstract class PreprocessingRule extends Rule {
+		private final Deque<Boolean> processingStack;
+
+		private PreprocessingRule(Deque<Boolean> processingStack) {
+			this.processingStack = processingStack;
+		}
+
+		@Override
+		public void begin(String namespace, String name,
+				Attributes attributes) throws Exception {
+			boolean toProcess = isProcessable(attributes);
+			processingStack.push(toProcess);
+		}
+
+		public abstract boolean isProcessable(Attributes attributes);
+
+		@Override
+		public void end(String namespace, String name) {
+			processingStack.pop();
+		}
+	}
+
+	private static class TransmissionBuilder {
+    	Calendar previousTime;
+    	Date time;
+    	String title;
+    	String episode;
+    	String description;
+    	String url;
+    	
+    	public TransmissionBuilder() {
+		}
+    	
+    	public TransmissionBuilder setStartingTime(Date time) {
+    		previousTime = Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"));
+    		previousTime.setTime(time);
+    		return this;
+    	}
+    	
+    	public TransmissionBuilder prepareForNextTransmission() {
+    		time = null;
+    		title = null;
+    		episode = null;
+    		description = null;
+    		url = null;
+    		return this;
+    	}
+    	
+    	public TransmissionBuilder setTime(int hour, int minute) {
+    		int oldHour = previousTime.get(Calendar.HOUR);
+    		int oldMinute = previousTime.get(Calendar.MINUTE);
+    		previousTime.set(Calendar.HOUR_OF_DAY, hour);
+    		previousTime.set(Calendar.MINUTE, minute);
+    		if (oldHour > hour || (oldHour == hour && oldMinute > minute)) {
+    			previousTime.add(Calendar.DATE, 1);
+    		}
+    		time = previousTime.getTime();
+    		return this;
+    	}
+    	
+    	public TransmissionBuilder setTitle(String title) {
+			this.title = title;
+			return this;
+		}
+    	
+    	public TransmissionBuilder setDescription(String description) {
+			this.description = description;
+			return this;
+		}
+    	
+    	public TransmissionBuilder setEpisode(String episode) {
+			this.episode = episode;
+			return this;
+		}
+    	
+    	public TransmissionBuilder setUrl(String url) {
+			this.url = url;
+			return this;
+		}
+    	
+    	public Transmission build() {
+    		return new Transmission(title, (episode != null ? episode + " - " : "") + description, time, null, url);
+    	}
     }
 }
