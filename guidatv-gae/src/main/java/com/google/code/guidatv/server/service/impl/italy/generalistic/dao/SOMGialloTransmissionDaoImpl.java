38a3cec4e39787a7679eb0fc1c9b6f9621f16cdc
antonio.petrelli <antonio.petrelli@a2f57c54-68f8-aa68-6b43-d9ab637a1aa9>
2012-07-08 19:44:46 +0000

    Fixed Giallo.

--- guidatv-gae/src/main/java/com/google/code/guidatv/server/service/impl/italy/generalistic/dao/SOMGialloTransmissionDaoImpl.java
+++ guidatv-gae/src/main/java/com/google/code/guidatv/server/service/impl/italy/generalistic/dao/SOMGialloTransmissionDaoImpl.java
@@ -1,47 +1,152 @@
 package com.google.code.guidatv.server.service.impl.italy.generalistic.dao;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
-import java.text.ParseException;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
+import java.util.Deque;
 import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import org.cyberneko.html.parsers.DOMParser;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Text;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
+import org.apache.commons.digester3.Digester;
+import org.apache.commons.digester3.Rule;
+import org.xml.sax.Attributes;
 
 import com.google.code.guidatv.model.Channel;
 import com.google.code.guidatv.model.Transmission;
-import com.google.code.guidatv.server.service.GuidaTvException;
+import com.google.code.guidatv.server.html.HTMLMultiDayParserDao;
+import com.google.code.guidatv.server.html.MultiDayTransmissionBuilder;
 
-public class SOMGialloTransmissionDaoImpl {
+public class SOMGialloTransmissionDaoImpl extends HTMLMultiDayParserDao {
 
 	private String pattern = "http://www.giallotv.it/programmi/?start=";
 
-	public Map<Date, List<Transmission>> getTransmissions(Channel channel,
-			Date day) {
+	@Override
+	protected void configure(final Map<Date, List<Transmission>> transmissionMap,
+			Digester digester, final MultiDayTransmissionBuilder builder) {
+		final Deque<Integer> processingStack = new ArrayDeque<Integer>();
+		digester.addRule("*/DIV/DIV", new Rule() {
+			@Override
+			public void begin(String namespace, String name,
+					Attributes attributes) throws Exception {
+				String id = attributes.getValue("id");
+				processingStack.push((id != null && id.startsWith("giorno")) ? 1 : 2);
+			}
+			
+			@Override
+			public void end(String namespace, String name) throws Exception {
+				if (processingStack.pop() == 1) {
+					builder.increaseDay();
+				}
+			}
+		});
+		digester.addRule("*/DIV/DIV/DIV/P", new Rule() {
+			@Override
+			public void begin(String namespace, String name,
+					Attributes attributes) throws Exception {
+				boolean toProcess = !processingStack.isEmpty() && processingStack.peek() > 0;
+				if (toProcess) {
+					builder.prepareForNextTransmission();
+				}
+				processingStack.push(toProcess ? 1 : 0);
+			}
+			
+			@Override
+			public void end(String namespace, String name) throws Exception {
+				Date currentDay = builder.getCurrentDay();
+				List<Transmission> transmissions = transmissionMap.get(currentDay);
+				if (transmissions == null) {
+					transmissions = new ArrayList<Transmission>();
+					transmissionMap.put(currentDay, transmissions);
+				}
+				transmissions.add(builder.build());
+				processingStack.pop();
+			}
+		});
+		digester.addRule("*/DIV/DIV/DIV/P/SPAN", new Rule() {
+			
+			private int mode = 0;
+			private DecimalFormat numberFormat;
+
+			{
+				numberFormat = (DecimalFormat) NumberFormat.getInstance();
+				numberFormat.applyPattern("##");
+			}
+			
+			@Override
+			public void begin(String namespace, String name,
+					Attributes attributes) throws Exception {
+				if (processingStack.peek() > 0) {
+					String clazz = attributes.getValue("class");
+					if ("orario".equals(clazz)) {
+						mode = 1;
+					} else if ("programma".equals(clazz)) {
+						mode = 2;
+					} else if ("info".equals(clazz)) {
+						mode = 3;
+					} else if ("episodio".equals(clazz)) {
+						mode = 4;
+					}
+				}
+			}
+			
+			@Override
+			public void body(String namespace, String name, String text)
+					throws Exception {
+				switch (mode) {
+					case 1:
+					String[] timeStrings = text.trim().split(":");
+					int hour = numberFormat.parse(timeStrings[0]).intValue();
+						int minutes = numberFormat.parse(timeStrings[1]).intValue();
+						builder.setTime(hour, minutes);
+						break;
+					case 2:
+						builder.setTitle(text.replaceAll("\\s*-\\s*$", ""));
+						break;
+					case 3:
+						builder.setDescription(text);
+						break;
+					case 4:
+						builder.setEpisode(text);
+				}
+			}
+			
+			@Override
+			public void end(String namespace, String name) throws Exception {
+				mode = 0;
+			}
+		});
+		
+		digester.addRule("*/DIV/DIV/DIV/P/SPAN/SPAN", new Rule() {
+			private boolean toProcess = false;
+			
+			@Override
+			public void begin(String namespace, String name,
+					Attributes attributes) throws Exception {
+				toProcess = processingStack.peek() > 0 && "sottotitolo".equals(attributes.getValue("class"));
+			}
+			
+			@Override
+			public void body(String namespace, String name, String text)
+					throws Exception {
+				if (toProcess) {
+					builder.setSubtitle(text);
+				}
+			}
+			
+			@Override
+			public void end(String namespace, String name) throws Exception {
+				toProcess = false;
+			}
+		});
+		
+	}
+
+	@Override
+	protected String getUrl(Channel channel, Date day) {
 		TimeZone timeZone = TimeZone.getTimeZone("Europe/Rome");
 		TimeZone requestTimeZone = TimeZone.getTimeZone("UTC");
 		Calendar currentCal = Calendar.getInstance(timeZone);
@@ -54,158 +159,7 @@ public class SOMGialloTransmissionDaoImpl {
 		requestCal.set(Calendar.MINUTE, 0);
 		requestCal.set(Calendar.SECOND, 0);
 		requestCal.set(Calendar.MILLISECOND, 0);
-		String urlString = pattern + Long.toString(requestCal.getTimeInMillis() / 1000);
-		Reader reader = null;
-		Map<Date, List<Transmission>> transmissionMap = new HashMap<Date, List<Transmission>>();
-		try {
-			URL url = new URL(urlString);
-			InputStream is = url.openStream();
-			reader = new InputStreamReader(is, "UTF-8");
-			DOMParser parser = new DOMParser();
-			parser.parse(new InputSource(reader));
-			Document document = parser.getDocument();
-			XPath xpath = XPathFactory.newInstance().newXPath();
-			XPathExpression expression = xpath
-					.compile("//DIV[starts-with(@id, 'giorno')]");
-			NodeList dayNodeList = (NodeList) expression.evaluate(document,
-					XPathConstants.NODESET);
-			DecimalFormat numberFormat = (DecimalFormat) NumberFormat
-					.getInstance();
-			numberFormat.applyPattern("00");
-			Calendar dayCal = Calendar.getInstance(timeZone);
-			dayCal.setTime(day);
-			Date currentDate = dayCal.getTime();
-			for (int d = 0; d < dayNodeList.getLength(); d++) {
-				Node dayNode = dayNodeList.item(d);
-				transmissionMap.put(currentDate,
-						buildDay(currentDate, dayNode, numberFormat));
-				dayCal.add(Calendar.DATE, 1);
-				currentDate = dayCal.getTime();
-			}
-		} catch (MalformedURLException e) {
-			throw new GuidaTvException(e);
-		} catch (IOException e) {
-			throw new GuidaTvException(e);
-		} catch (SAXException e) {
-			throw new GuidaTvException(e);
-		} catch (XPathExpressionException e) {
-			throw new GuidaTvException(e);
-		} catch (ParseException e) {
-			throw new GuidaTvException(e);
-		} finally {
-			if (reader != null) {
-				try {
-					reader.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		return transmissionMap;
-	}
-
-	private List<Transmission> buildDay(Date currentDate, Node dayNode,
-			DecimalFormat numberFormat) throws ParseException {
-		NodeList superNodeList = dayNode.getChildNodes();
-		List<Transmission> transmissions = new ArrayList<Transmission>();
-		for (int v = 0; v < superNodeList.getLength(); v++) {
-			Node partDayNode = superNodeList.item(v);
-			if (partDayNode instanceof Element) {
-				Element partDayElement = (Element) partDayNode;
-				if ("DIV".equals(partDayElement.getTagName())
-						&& "tabdiv"
-								.equals(partDayElement.getAttribute("class"))) {
-					TimeZone timeZone = TimeZone.getTimeZone("Europe/Rome");
-					Calendar cal = Calendar.getInstance(timeZone);
-					cal.setTime(currentDate);
-					NodeList nodeList = partDayElement.getChildNodes();
-					for (int i = 0; i < nodeList.getLength(); i++) {
-						Node item = nodeList.item(i);
-						if (item instanceof Element) {
-							Element superelement = (Element) item;
-							if ("P".equals(superelement.getTagName())) {
-								Transmission transmission = buildTransmission(superelement,
-										currentDate, cal, i, numberFormat);
-								transmissions.add(transmission);
-							}
-						}
-					}
-				}
-			}
-		}
-		return transmissions;
-	}
-
-	private void loadDayPart(Node item, Date currentDate,
-			DecimalFormat numberFormat, List<Transmission> transmissions, int i)
-			throws ParseException {
-		TimeZone timeZone = TimeZone.getTimeZone("Europe/Rome");
-		Calendar cal = Calendar.getInstance(timeZone);
-		cal.setTime(currentDate);
-		NodeList children = item.getChildNodes();
-		for (int j = 0; j < children.getLength(); j++) {
-			Node supernode = children.item(j);
-			if (supernode instanceof Element) {
-				Element superelement = (Element) supernode;
-				if ("P".equals(superelement.getTagName())) {
-					Transmission transmission = buildTransmission(superelement,
-							currentDate, cal, i, numberFormat);
-					transmissions.add(transmission);
-				}
-			}
-		}
-	}
-
-	private Transmission buildTransmission(Element superelement,
-			Date currentDate, Calendar cal, int i, DecimalFormat numberFormat)
-			throws ParseException {
-		Date start = null;
-		String name = null;
-		String link = null;
-		String episode = null;
-		String description = null;
-		NodeList subchildren = superelement.getChildNodes();
-		for (int t = 0; t < subchildren.getLength(); t++) {
-			Node node = subchildren.item(t);
-			if (node instanceof Element) {
-				Element element = (Element) node;
-				Node firstChild = element.getFirstChild();
-				if ("SPAN".equals(element.getTagName())) {
-					String className = element.getAttribute("class");
-					if ("orario".equals(className)) {
-						Text text = (Text) firstChild;
-						String timeString = text.getWholeText().trim();
-						String hour = timeString.substring(0, 2);
-						String minutes = timeString.substring(3, 5);
-						cal.setTime(currentDate);
-						cal.set(Calendar.HOUR_OF_DAY, 0);
-						cal.set(Calendar.MINUTE, 0);
-						cal.set(Calendar.SECOND, 0);
-						cal.set(Calendar.MILLISECOND, 0);
-						int hours = numberFormat.parse(hour).intValue();
-						int minute = numberFormat.parse(minutes).intValue();
-						if (hours < 6 || (hours == 6 && minute == 0 && i != 0)) {
-							cal.add(Calendar.DAY_OF_YEAR, 1);
-						}
-						cal.set(Calendar.HOUR_OF_DAY, hours);
-						cal.set(Calendar.MINUTE, minute);
-						start = cal.getTime();
-					} else if ("programma".equals(className)) {
-						Text text = (Text) firstChild;
-						name = text.getWholeText();
-					} else if ("info".equals(className)) {
-						Text text = (Text) firstChild;
-						episode = text.getWholeText();
-					} else if ("episodio".equals(className)) {
-						Text text = (Text) firstChild;
-						description = text.getWholeText();
-					}
-				}
-			}
-		}
-		Transmission transmission = new Transmission(name, episode + " "
-				+ description, start, null, link);
-		return transmission;
+		return pattern + Long.toString(requestCal.getTimeInMillis() / 1000);
 	}
 
 }
